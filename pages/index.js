18d5c70ad1a3237a15c9c391a72c6493843d8cfd
Arjan Hoek <arjanhoek5191@gmail.com>
2022-03-20 13:08:14 +0100

    add dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Head from 'next/head';
 import Link from 'next/link';
+
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
+
+import { getSortedPostsData } from '../lib/posts.js';
+
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,9 +25,22 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, this is my first website created with Next.js</p>
-        <Link href="/posts/first-post">
-          <a>Go to my first post</a>
-        </Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
