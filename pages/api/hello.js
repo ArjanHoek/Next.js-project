23e7ea5ed45fa747d22ba09b9fb545ae8286e307
Arjan Hoek <arjanhoek5191@gmail.com>
2022-03-20 15:24:41 +0100

    simplify users api even more

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,3 @@
 export default async function handler(req, res) {
-  console.log(process.env.DB_LINK);
   res.status(200).json({ text: 'Hello' });
 }
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,34 +1,38 @@
-import dbConnect from '../../lib/dbConnect';
-import User from '../../models/User';
-
 export default async function handler(req, res) {
-  const { method } = req;
-
-  res.status(200).json({ test: true });
-
-  return;
-
-  await dbConnect();
-
-  switch (method) {
-    case 'GET':
-      try {
-        const users = await User.find({});
-        res.status(200).json({ success: true, data: users });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    case 'POST':
-      try {
-        const user = await User.create(req.body);
-        res.status(201).json({ success: true, data: user });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
+  res.status(200).json({ text: 'Users' });
 }
+
+// import dbConnect from '../../lib/dbConnect';
+// import User from '../../models/User';
+
+// export default async function handler(req, res) {
+//   const { method } = req;
+
+//   res.status(200).json({ test: true });
+
+//   return;
+
+//   await dbConnect();
+
+//   switch (method) {
+//     case 'GET':
+//       try {
+//         const users = await User.find({});
+//         res.status(200).json({ success: true, data: users });
+//       } catch (error) {
+//         res.status(400).json({ success: false });
+//       }
+//       break;
+//     case 'POST':
+//       try {
+//         const user = await User.create(req.body);
+//         res.status(201).json({ success: true, data: user });
+//       } catch (error) {
+//         res.status(400).json({ success: false });
+//       }
+//       break;
+//     default:
+//       res.status(400).json({ success: false });
+//       break;
+//   }
+// }
