c30e73350d15880d2daef4943aa521390a28c7b3
Arjan Hoek <arjanhoek5191@gmail.com>
2022-03-26 16:48:24 +0100

    remove unnecessary packages

--- pages/lodges/[id].js
+++ pages/lodges/[id].js
@@ -4,8 +4,6 @@ import utilStyles from '../../styles/utils.module.css';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
-import Date from '../../components/date.js';
-
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   return {
@@ -31,10 +29,6 @@ export default function Post({ postData }) {
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
   );
